66ae12cc1f621b6bebad37925dd2a1cac0b6eff9
Rovers11 <spandanmukherjeegithub@gmail.com>
2024-09-04 23:15:48 +0530

    chore: Remove unused component imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import styles from "@/styles/style";
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "@/components"
+import { Navbar, Hero, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "@/components"
 const Home: React.FC = () => {
   return (
     <>
